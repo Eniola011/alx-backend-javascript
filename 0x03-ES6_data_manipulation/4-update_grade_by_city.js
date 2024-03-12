b27089400078e048129c9e75e800cfd91b822f1a
Eniola011 <yettyagbalu@gmail.com>
2024-03-12 16:27:35 +0000

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+// Get an array of students for a specific city with their new grade.
+function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || typeof city !== 'string' || !Array.isArray(newGrades)) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
+
+export default updateStudentGradeByCity;
