4ebb22047d9f405c69ce47c79e076d5c384de863
Eniola011 <yettyagbalu@gmail.com>
2024-03-12 13:17:00 +0000

    /* eslint-disable no-constant-condition */

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,13 @@
 // Return a Promise using this prototype function getResponseFromAPI()
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-      if (true) {
-        resolve();
-      } else {
-        reject();
-      }
-});
+    /* eslint-disable no-constant-condition */
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
 
 export default getResponseFromAPI;
