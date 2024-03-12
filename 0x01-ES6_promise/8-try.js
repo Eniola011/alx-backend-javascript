5cac99a97041c85548c2648c7b0cf0f6f8b5036a
Eniola011 <yettyagbalu@gmail.com>
2024-03-12 09:42:24 +0000

    task 8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+// throw error / try catch
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+
+  const result = numerator / denominator;
+  return result;
+}
