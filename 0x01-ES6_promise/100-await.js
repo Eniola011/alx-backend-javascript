579501cdc42eb1e3c78bd78ae3b82389e1d0908c
Eniola011 <yettyagbalu@gmail.com>
2024-03-12 10:39:30 +0000

    await/async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+// await and async
+import { uploadPhoto, createUser } from './utils.js';
+
+const asyncUploadUser = async () => {
+  let picture;
+  let user;
+
+  try {
+    picture = await uploadPhoto();
+    user = await createUser();
+  } catch (e) {
+    picture = null;
+    user = null;
+  }
+  return ({ picture, user });
+}
+
+export default asyncUploadUser;
