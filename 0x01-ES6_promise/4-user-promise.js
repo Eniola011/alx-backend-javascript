8cba07cbe8914ecb4fc1dfd4ee402081e7f6f211
Eniola011 <yettyagbalu@gmail.com>
2024-03-12 14:10:36 +0000

    eslint issues

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,11 @@
 // single promise
 function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
-  });
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
 
 export default signUpUser;
