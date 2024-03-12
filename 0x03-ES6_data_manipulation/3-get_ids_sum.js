4fe1ec217b67b061674835d1cd28c793864640c2
Eniola011 <yettyagbalu@gmail.com>
2024-03-12 16:11:05 +0000

    sum of all the student ids.

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+// Get the sum of all the student ids.
+function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.reduce((sum, student) => sum + student.id, 0);
+}
+
+export default getStudentIdsSum;
